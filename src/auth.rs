7a8b1ead12847e9dea1cab1ca7f8eecc5309ab17
Sumit Modak <152054612+sumit-ftr@users.noreply.github.com>
2024-04-11 21:21:43 +0530

    added README & updated auth.rs

--- src/auth.rs
+++ src/auth.rs
@@ -1,17 +1,62 @@
-use axum::{routing::get, Router};
+use axum::{
+    routing::{get, post},
+    Json, Router,
+};
+use serde::Deserialize;
 
 pub fn auth_routes() -> Router {
     Router::new()
-        .route("/", get(home))
+        .route("/", get(home_page))
         .route("/signup", get(signup_page).post(signup))
         .route("/login", get(login_page).post(login))
-        .route("/logout", get(logout_page).post(logout))
+        .route("/logout", post(logout))
 }
 
-pub async fn login() {}
-pub async fn signup() {}
-pub async fn logout() {}
-pub async fn home() {}
+#[derive(Deserialize, Debug)]
+pub struct LoginResponse {
+    cookie: String,
+    username: String,
+    password: String,
+}
+
+pub async fn login(Json(body): Json<LoginResponse>) {
+    println!("{body:?}");
+    crate::auth::home_page().await
+}
+
+#[derive(Deserialize, Debug)]
+enum Gender {
+    Male,
+    Female,
+    Other,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct SignupResponse {
+    email: String,
+    username: String,
+    password: String,
+    name: String,
+    gender: Gender,
+    dob: (u8, u8, u16),
+}
+
+pub async fn signup(Json(body): Json<SignupResponse>) {
+    println!("{body:?}");
+    crate::auth::home_page().await
+}
+
+#[derive(Deserialize, Debug)]
+pub struct LogoutResponse {
+    username: String,
+    cookie: String,
+}
+
+pub async fn logout(Json(body): Json<SignupResponse>) {
+    println!("{body:?}");
+    crate::auth::login_page().await
+}
+
+pub async fn home_page() {}
 pub async fn login_page() {}
 pub async fn signup_page() {}
-pub async fn logout_page() {}
