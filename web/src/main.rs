06064e524bce45d34383a8a3c6c553d873249739
Sumit Modak <152054612+sumitftr@users.noreply.github.com>
2025-11-03 22:12:58 +0530

    add: graceful shutdown on `SIGINT` and `SIGTERM`

--- web/src/main.rs
+++ web/src/main.rs
@@ -1,5 +1,4 @@
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
-// mod conn_info;
 
 pub static SOCKET: std::sync::LazyLock<String> =
     std::sync::LazyLock::new(|| std::env::var("SOCKET").unwrap());
@@ -21,6 +20,33 @@ async fn main() {
 
     tracing::info!("[+] listening on {}", listener.local_addr().unwrap());
     axum::serve(listener, router) // app.into_make_service_with_connect_info::<conn_info::ClientConnInfo>(),
+        .with_graceful_shutdown(shutdown_signal())
         .await
         .unwrap();
+}
+
+/// Shutdown signal to run axum with graceful shutdown when
+/// a user presses Ctrl+C or Unix sends a terminate signal.
+pub async fn shutdown_signal() {
+    let ctrl_c = async {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
 }
